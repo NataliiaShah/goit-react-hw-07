016d28886cb0450310be1bef1b7c3e98c3785726
NataliiaShah <gavrilovskaboroluk@gmail.com>
2025-01-27 20:16:04 +0200

    start

---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import "../../../node_modules/modern-normalize/modern-normalize.css";
+import css from "./App.module.css";
+
+import ContactForm from "../ContactForm/ContactForm";
+import ContactList from "../ContactList/ContactList";
+import SearchBox from "../SearchBox/SearchBox";
+
+function App() {
+  return (
+    <div className={css.container}>
+      <h1 className={css.mainTitle}>Phonebook 📱</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
